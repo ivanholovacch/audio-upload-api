c1617dd6d4917f83cfd42390ea0b9ccab8b08995
Ivan Holovach <ivanholovach@Ivans-Laptop.local>
2024-12-06 15:49:16 +0200

    fixed transcribtion saving

--- src/services/audioFile/saveTranscription.service.ts
+++ src/services/audioFile/saveTranscription.service.ts
@@ -1,12 +1,11 @@
 import fs from 'fs';
-import path from 'path';
+
+const TRANSCRIPTION_PATH = 'transcripts/transcription.txt';
 
 export async function saveTranscribedText(transcribedText: string) {
-  const filePath = path.join(__dirname, '../../transcripts', 'transcription.txt');
-
   try {
-    await fs.promises.writeFile(filePath, transcribedText);
-    console.log('Transcribed text saved to audioFile:', filePath);
+    await fs.promises.writeFile(TRANSCRIPTION_PATH, transcribedText);
+    console.log('Transcribed text saved to audioFile:', TRANSCRIPTION_PATH);
   } catch (error) {
     console.error('Error saving transcribed text:', error);
     throw error;
